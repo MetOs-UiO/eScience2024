14c182bbcd4a4c7400ca980a778ef5c9ed2b844e
Ove Haugvaldstad <ovehaugv@outlook.com>
2024-04-15 12:50:23 +0200

    Fix issue in config (#2)

--- docs/conf.py
+++ docs/conf.py
@@ -86,7 +86,7 @@ html_theme = "sphinx_book_theme"
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["css"]
-html_extra_path = ["css", "learning/notebooks"]
+
 
 html_context = {
     "display_github": True,
